56566cb99a5eaf5fabd971b130b8296343827c89
omgwtfrly <alan.bryntsev@hotmail.com>
2019-02-05 17:18:51 +0300

    SS result

--- dashboards/sprint_dashboard.py
+++ dashboards/sprint_dashboard.py
@@ -3,6 +3,7 @@ from adapters.issue_utils import get_domain_by_project, get_domain
 import math
 import plotly.plotly
 import plotly.graph_objs as go
+from datetime import datetime
 
 
 def domain_position(row, col, cols):
@@ -19,9 +20,9 @@ def domain_position(row, col, cols):
         3: [0.34, 0.5]
     }
     y_pos = {
-        1: [0.68, 1],
-        2: [0.34, 0.66],
-        3: [0, 0.32]
+        1: [0.74, 1],
+        2: [0.47, 0.73],
+        3: [0.2, 0.46]
     }
     return dict(
         x=x_pos[col],
@@ -29,17 +30,27 @@ def domain_position(row, col, cols):
     )
 
 
+def color_for_status(status):
+    return {
+        'Open': 'rgb(217,98,89)',
+        'In Fix': 'rgb(254,210,92)',
+        'Closed': 'rgb(29,137,49)'
+    }[status]
+
+
 class SprintDashboard(AbstractDashboard):
     auto_open, fixversion, repository = True, None, None
     key_list, project_list, status_list, components_list, timeoriginalestimate_list, timespent_list, issuetype_list = \
         [], [], [], [], [], [], []
-    domain_list, bugs_dict, accuracy_dict = [], {}, {}
+    domain_list, bugs_dict, accuracy_dict, all_bugs = [], {}, {}, {}
 
     def prepare(self, data):
         self.key_list, self.project_list, self.status_list, self.components_list, self.timeoriginalestimate_list,\
          self.timespent_list, self.issuetype_list = data.get_sprint_info(self.fixversion)
+        self.all_bugs = {'Open': 0, 'In Fix': 0, 'Closed': 0}
         for i in range(len(self.key_list)):
             if self.issuetype_list[i] == 'Bug':
+                self.all_bugs[self.status_list[i]] += 1
                 self.components_list[i] = self.components_list[i].split(',')
                 if len(self.components_list[i]) != 1:
                     for _ in range(1, len(self.components_list[i])):
@@ -62,10 +73,13 @@ class SprintDashboard(AbstractDashboard):
                 if self.components_list[i] != [''] and get_domain(*self.components_list[i]) != 'Others':
                     self.domain_list[i] = get_domain(*self.components_list[i])
                 else:
-                    self.domain_list[i] = get_domain_by_project(self.project_list[i])
+                    self.domain_list[i] = 'Indefinite'
+                    # self.domain_list[i] = get_domain_by_project(self.project_list[i])
                 if self.domain_list[i] not in self.bugs_dict.keys():
                     self.bugs_dict[self.domain_list[i]] = {'Open': 0, 'In Fix': 0, 'Closed': 0}
                 self.bugs_dict[self.domain_list[i]][self.status_list[i]] += 1
+        print(self.all_bugs)
+
 
     def export_to_plotly(self):
         if len(self.key_list) == 0:
@@ -73,6 +87,7 @@ class SprintDashboard(AbstractDashboard):
 
         plotly.tools.set_credentials_file(username='Rnd-Rnd', api_key='GFSxsbDP8rOiakf0rs8U')
 
+        print(self.bugs_dict)
         data = []
         cols = math.ceil(len(self.bugs_dict.keys()) / 3)
         for domain, i in zip(self.bugs_dict.keys(), range(len(self.bugs_dict.keys()))):
@@ -133,6 +148,24 @@ class SprintDashboard(AbstractDashboard):
                 borderwidth=2,
                 borderpad=4
             ))
+        base = 0
+        for status in self.all_bugs.keys():
+            data.append(go.Bar(
+                orientation='h',
+                y=['BSSBox'],
+                x=[self.all_bugs[status]],
+                xaxis='x2',
+                yaxis='y2',
+                name=status,
+                showlegend=False,
+                text='{}<br>{}'.format(status, self.all_bugs[status]),
+                textposition='auto',
+                base=base,
+                marker=dict(
+                    color=color_for_status(status)
+                )
+            ))
+            base += self.all_bugs[status]
 
         axis = dict()
         layout = dict(
@@ -141,10 +174,13 @@ class SprintDashboard(AbstractDashboard):
                 x=0.695,
                 y=1.05
             ),
-            title=self.dashboard_name + (' <sup>in cloud</sup>' if self.repository == 'online' else ''),
+            title='<b>{0} as of {1}</b>'.format(self.dashboard_name, datetime.now().strftime("%d.%m.%y %H:%M"))
+                  + (' <sup>in cloud</sup>' if self.repository == 'online' else ''),
             annotations=annotations,
             xaxis1=dict(axis, **dict(domain=[0.55, 1], anchor='y1')),
-            yaxis1=dict(axis, **dict(domain=[0, 1]), anchor='x1', ticksuffix='  ')
+            yaxis1=dict(axis, **dict(domain=[0, 1], anchor='x1', ticksuffix='  ')),
+            xaxis2=dict(axis, **dict(domain=[0, 0.5], anchor='y2')),
+            yaxis2=dict(axis, **dict(domain=[0, 0.19], anchor='x2', ticksuffix='  '))
         )
 
         title = self.dashboard_name
