0994eadcf6d4e482149856125c6243fec76a56b4
omgwtfrly <alan.bryntsev@hotmail.com>
2019-02-06 20:35:08 +0300

    BSSBOX bugs tracking

--- dashboards/bssbox_bugs_tracking_dashboard.py
+++ dashboards/bssbox_bugs_tracking_dashboard.py
@@ -4,6 +4,7 @@ import datetime
 import numpy
 import plotly
 import plotly.graph_objs as go
+import math
 
 
 def alert_action(days, priorities):
@@ -22,14 +23,38 @@ def alert_action(days, priorities):
     return color
 
 
+def domain_position(row, col, rows):
+    delta = 0.02
+    length = ((0.99-(rows-1)*delta)/rows)
+    start, end, y_pos = 0.99-length, 0.99, {}
+    for i in range(1, rows+1):
+        y_pos[i] = [start, end]
+        end = start - delta
+        start = end - length
+    x_pos = {
+        1: [0.75, 0.87],
+        2: [0.88, 1]
+    }
+    return dict(
+        x=x_pos[col],
+        y=y_pos[row]
+    )
+
+
 class BssboxBugsTrackingDashboard(AbstractDashboard):
     auto_open, repository = True, None
     tracking_data, pivot_data = {}, {}
 
     def prepare(self, data):
         self.tracking_data = {key: [] for key in list(data.keys()) if key != 'Resolved'}
+        self.pivot_data['BSSBox'] = {'Punctually': 0, 'Overdue': 0}
+        preparing_data = {key: [] for key in data.keys()}
         for i in range(len(data['Key'])):
-            print('{}: {}'.format(data['Domain'][i], get_domain(data['Domain'][i])))
+            if get_domain(data['Domain'][i]) != 'Others':
+                for key in data.keys():
+                    preparing_data[key].append(data[key][i])
+        data = preparing_data
+        for i in range(len(data['Key'])):
             data['Domain'][i] = get_domain(data['Domain'][i]) if data['Domain'][i] is not None else 'Indefinite'
             data['Days on fix'][i] = int(numpy.busday_count(data['Days on fix'][i], datetime.datetime.now().date())+1)\
                 if data['Status'][i] not in ('Closed', 'Resolved')\
@@ -43,13 +68,17 @@ class BssboxBugsTrackingDashboard(AbstractDashboard):
                 if data['Priority'][i] == 'Blocker':
                     if data['Days on fix'][i] > 1:
                         self.pivot_data[data['Domain'][i]]['Overdue'] += 1
+                        self.pivot_data['BSSBox']['Overdue'] += 1
                     else:
                         self.pivot_data[data['Domain'][i]]['Punctually'] += 1
+                        self.pivot_data['BSSBox']['Punctually'] += 1
                 elif data['Priority'][i] == 'Critical':
                     if data['Days on fix'][i] > 2:
                         self.pivot_data[data['Domain'][i]]['Overdue'] += 1
+                        self.pivot_data['BSSBox']['Overdue'] += 1
                     else:
                         self.pivot_data[data['Domain'][i]]['Punctually'] += 1
+                        self.pivot_data['BSSBox']['Punctually'] += 1
         print(self.pivot_data)
 
     def export_to_plotly(self):
@@ -62,31 +91,63 @@ class BssboxBugsTrackingDashboard(AbstractDashboard):
         cells_values = [value for value in self.tracking_data.values()]
         data = [go.Table(
             domain=dict(
-                x=[0, 1],
-                y=[0.3, 1]
+                x=[0, 0.72],
+                y=[0, 1]
             ),
             columnorder=[1, 2, 3, 4, 5, 6, 7],
-            columnwidth=[2, 14, 2, 2, 2, 2, 2],
+            columnwidth=[3, 12, 3, 2, 2, 2, 2],
             header=dict(
                 values=header_values,
                 fill=dict(color=['grey']),
-                font=dict(color='white')
+                font=dict(color='white'),
+                line=dict(width=2)
             ),
             cells=dict(
                 values=cells_values,
                 align=['center', 'left', 'center', 'center', 'center', 'center', 'center'],
                 fill=dict(color=alert_action(days=self.tracking_data['Days on fix'],
                                              priorities=self.tracking_data['Priority'])),
-                height=25
+                height=25,
+                line=dict(width=2)
             )
         )]
+        rows = math.ceil(len(self.pivot_data.keys()) / 2)
+        for domain, i in zip(self.pivot_data.keys(), range(len(self.pivot_data.keys()))):
+            row, col = int((i % rows) + 1), int((i // rows) + 1)
+            data.append(go.Pie(
+                labels=list(self.pivot_data[domain].keys()),
+                values=list(self.pivot_data[domain].values()),
+                hoverinfo='label+percent',
+                textinfo='label+value',
+                textposition='inside',
+                hole=0.4,
+                domain=domain_position(row, col, rows),
+                marker=dict(
+                    colors=['rgb(232,232,232)', 'rgb(255,204,204)'],
+                    line=dict(width=1)
+                ),
+                showlegend=False,
+                title=domain,
+                titleposition='middle center'
+            ))
 
         title = self.dashboard_name
         html_file = '//billing.ru/dfs/incoming/ABryntsev/' + "{0}.html".format(title)
 
         layout = go.Layout(
             title=title,
-            font=dict(family='Oswald, sans-serif', size=12)
+            font=dict(family='Oswald, sans-serif', size=12),
+            shapes=[dict(
+                type='rect',
+                x0=0.75,
+                y0=-0.01,
+                x1=1,
+                y1=1,
+                line=dict(
+                    color='rgb(0,0,0)',
+                    width=1
+                )
+            )]
         )
 
         fig = go.Figure(data=data, layout=layout)
